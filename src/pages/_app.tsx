872f41ef2f752e0d09bcefeb403b29b562dab592
blucin <glaivesharp@gmail.com>
2023-05-21 15:50:38 +0530

    Change auth provider session wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
-import { type AppType } from "next/app";
-import { ClerkProvider } from "@clerk/nextjs";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app"
+import type { Session } from "next-auth";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return(
-    <ClerkProvider {...pageProps}>
+const MyApp= ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{ session: Session }>) => {
+  return (
+    <SessionProvider session={session}>
       <Component {...pageProps} />
-    </ClerkProvider>
+    </SessionProvider>
   );
 };
 
