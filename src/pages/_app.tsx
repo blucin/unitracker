9e653bda5ba0c0d810bfc3fdd6e126a62f81c788
blucin <glaivesharp@gmail.com>
2023-05-28 16:23:37 +0530

    Modify session wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,26 @@
-import { SessionProvider } from "next-auth/react";
+import { getSession, SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app"
 import type { Session } from "next-auth";
 import { api } from "~/utils/api";
+import type { NextPageContext } from "next";
 
 import "~/styles/globals.css";
 
 const MyApp= ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps,
 }: AppProps<{ session: Session }>) => {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
+};
+
+MyApp.getInitialProps = async (ctx: NextPageContext) => {
+  return {
+    session: await getSession(ctx),
+  };
 };
 
 export default api.withTRPC(MyApp);
