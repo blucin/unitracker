be5ee37f21339abf3b536a2ff923bebc4109e72a
blucin <glaivesharp@gmail.com>
2023-05-30 14:28:45 +0530

    Separate nav from layout

--- components/DashboardLayout.tsx
+++ components/DashboardLayout.tsx
@@ -1,37 +1,11 @@
-import { cn } from "~/lib/utils";
-//import { ScrollArea } from "@/components/ui/scroll-area";
-import { Button } from "@/components/ui/button";
+import { DashboardNavBar } from "@/components/DashboardNavBar";
+//import { cn } from "~/lib/utils";
 
-export function DashboardLayout({
-  className,
-}: React.HTMLAttributes<HTMLElement>) {
+export function DashboardLayout({children}: {children: React.ReactNode}) {
   return(
-    <div className={cn("pb-12", className)}>
-      <div className="space-y-4 py-4">
-        <div className="px-4 py-2">
-          <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
-            Discover
-          </h2>
-          <div className="space-y-1">
-            <Button
-              variant="secondary"
-              size="sm"
-              className="w-full justify-start"
-            >
-              
-              Listen Now
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              
-              Browse
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              
-              Radio
-            </Button>
-          </div>
-        </div>
-      </div>
+    <div className="flex">
+      <DashboardNavBar />
+      <main>{children}</main>
     </div>
   )
 }
---
+++ components/DashboardNavBar.tsx
@@ -0,0 +1,34 @@
+import { cn } from "~/lib/utils";
+//import { ScrollArea } from "@/components/ui/scroll-area";
+import { Button } from "@/components/ui/button";
+
+export function DashboardNavBar({
+    className,
+}: React.HTMLAttributes<HTMLElement>) {
+    return (
+        <div className={cn("pb-12", className)}>
+            <div className="space-y-4 py-4">
+                <div className="px-4 py-2">
+                    <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
+                        Discover
+                    </h2>
+                    <div className="space-y-1">
+                        <Button
+                            variant="secondary"
+                            size="sm"
+                            className="w-full justify-start"
+                        >
+                            Listen Now
+                        </Button>
+                        <Button variant="ghost" size="sm" className="w-full justify-start">
+                            Browse
+                        </Button>
+                        <Button variant="ghost" size="sm" className="w-full justify-start">
+                            Radio
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
