148e8d868e9c90856930e882836b17c82658b28f
blucin <glaivesharp@gmail.com>
2023-05-31 15:56:39 +0530

    Test

--- components/DashboardLayout.tsx
+++ components/DashboardLayout.tsx
@@ -1,5 +1,6 @@
 import { DashboardNavBar } from "@/components/DashboardNavBar";
 import { SiteHeader } from "@/components/SiteHeader";
+import { Separator } from "@/components/ui/separator";
 //import { cn } from "~/lib/utils";
 
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
@@ -7,9 +8,16 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
     <>
       <SiteHeader />
       <div className="flex">
-        <DashboardNavBar />
-        <main>{children}</main>
+        <DashboardNavBar/>
+        <main className="p-5">{children}</main>
       </div>
     </>
   );
 }
+
+/*
+<div className="hidden lg:flex">
+          <DashboardNavBar/>
+          <Separator/>
+        </div>
+        */
