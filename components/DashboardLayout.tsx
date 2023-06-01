98598c0fcf3b3afa6eef2090626ac303b836ece2
blucin <glaivesharp@gmail.com>
2023-06-01 19:41:23 +0530

    Finalize dashboard layout

--- components/DashboardLayout.tsx
+++ components/DashboardLayout.tsx
@@ -1,23 +1,14 @@
 import { DashboardNavBar } from "@/components/DashboardNavBar";
 import { SiteHeader } from "@/components/SiteHeader";
-import { Separator } from "@/components/ui/separator";
-//import { cn } from "~/lib/utils";
 
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="h-screen">
       <SiteHeader />
-      <div className="flex">
-        <DashboardNavBar/>
-        <main className="p-5">{children}</main>
+      <div className="grid lg:grid-cols-6">
+        <DashboardNavBar className="border-r h-screen"/>
+        <main className="p-10 col-span-5">{children}</main>
       </div>
-    </>
+    </div>
   );
 }
-
-/*
-<div className="hidden lg:flex">
-          <DashboardNavBar/>
-          <Separator/>
-        </div>
-        */
