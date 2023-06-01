9af23cfffda15ff3415c6730b32a4e7db8e0e5d3
blucin <glaivesharp@gmail.com>
2023-06-01 19:44:17 +0530

    Export router input and router output types

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { attendanceRouter } from "./routers/attendance";
+import { subjectRouter } from "./routers/subject";
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 
 /**
  * This is the primary router for your server.
@@ -8,7 +10,10 @@ import { attendanceRouter } from "./routers/attendance";
  */
 export const appRouter = createTRPCRouter({
   attendance: attendanceRouter,
+  subject: subjectRouter,
 });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
+export type RouterInput = inferRouterInputs<AppRouter>;
+export type RouterOutput = inferRouterOutputs<AppRouter>;
