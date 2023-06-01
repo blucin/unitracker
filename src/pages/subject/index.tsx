97ef5bca141884b892e716225c1264addce2a8b1
blucin <glaivesharp@gmail.com>
2023-06-01 19:43:46 +0530

    Add subject table to the view subject route

--- src/pages/subject/index.tsx
+++ src/pages/subject/index.tsx
@@ -1,28 +1,45 @@
 import { useSession } from "next-auth/react";
 import { DashboardLayout } from "@/components/DashboardLayout";
-import type { NextPageWithLayout } from '~/pages/_app';
+import type { NextPageWithLayout } from "~/pages/_app";
+import { Separator } from "@/components/ui/separator";
+import { SubjectTable } from "@/components/SubjectTable";
+import { api } from "~/utils/api";
+import { LoadingTable } from "@/components/LoadingTable";
 
-const Subject:NextPageWithLayout = () => {
+const Subject: NextPageWithLayout = () => {
   useSession({
     required: true,
     onUnauthenticated() {
       return { redirectTo: "/login" };
     },
   });
+  const { data, isLoading } = api.subject.getAll.useQuery();
+  const { mutate } = api.subject.deleteById.useMutation();
+
+  const handleDelete = (_id: string) => {
+    mutate({ id: _id });
+  };
+  
   return (
     <>
-      <p> View Subject </p>
-      <p> Labore maxime optio temporibus reiciendis, quas illo? Ipsam error repudiandae ducimus nemo minima eos, id esse molestiae est! Natus pariatur ipsum magnam!</p>
+      <div className="space-y-2">
+        <h2 className="text-2xl font-bold tracking-tight"> Subjects </h2>
+        <p className="text-muted-foreground">
+          View and manage your subjects here.
+        </p>
+      </div>
+      <Separator className="my-5" />
+      {isLoading ? (
+        <LoadingTable rows={10} cols={3} />
+      ) : (
+        <SubjectTable data={data} handleDelete={handleDelete}/>
+      )}
     </>
   );
-}
+};
 
 Subject.getLayout = function getLayout(page: React.ReactElement) {
-  return (
-    <DashboardLayout>
-      {page}
-    </DashboardLayout>
-  );
+  return <DashboardLayout>{page}</DashboardLayout>;
 };
 
 export default Subject;
