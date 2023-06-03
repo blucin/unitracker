3eaec2a7b367acac02f593add5006e51afb1af25
akshar <94466270+blucin@users.noreply.github.com>
2023-06-03 12:06:49 +0000

    Edit timetable router to use drizzle

--- src/server/api/routers/timetable.ts
+++ src/server/api/routers/timetable.ts
@@ -1,11 +1,14 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import _ from "lodash";
-
+import { timeTable } from "~/drizzle/out/schema";
 
 export const timeTableRouter = createTRPCRouter({
   getAll: protectedProcedure.query(async ({ ctx }) => {
-    const timeTables = await ctx.db.select().from(tim)
+    const timeTables = await ctx.db
+      .select()
+      .from(timeTable)
+      .where(eq(timeTable.userId, ctx.session.user.id));
     return timeTables;
   }),
 });
