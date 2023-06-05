309501e5480a53b392534c5dee1743b1dde9c07f
akshar <94466270+blucin@users.noreply.github.com>
2023-06-05 16:43:44 +0000

    Fix mobile screen zoom out issue

--- components/DashboardLayout.tsx
+++ components/DashboardLayout.tsx
@@ -3,9 +3,9 @@ import { SiteHeader } from "@/components/SiteHeader";
 
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen">
+    <div className="relative flex min-h-screen flex-col">
       <SiteHeader />
-      <div className="grid lg:grid-cols-6">
+      <div className="flex-1 lg:grid grid-cols-6">
         <DashboardNavBar className="border-r h-screen"/>
         <main className="p-10 col-span-5">{children}</main>
       </div>
