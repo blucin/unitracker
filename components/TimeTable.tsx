ba6cabfc98c547778b480b4d56d97f9979b0020d
akshar <94466270+blucin@users.noreply.github.com>
2023-06-05 11:43:03 +0000

    Add timetable components and index page

--- components/TimeTable.tsx
+++ components/TimeTable.tsx
@@ -1,12 +1,3 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { cn } from "~/lib/utils";
 import type { RouterOutput } from "~/server/api/root";
 
@@ -18,7 +9,24 @@ type TimeTableProps = {
 export function TimeTable({ ...props }: TimeTableProps) {
   return (
     <>
-      <p>temp</p>
+      {props.data &&
+        Object.entries(props.data).map(([timetableName, timetable]) => (
+          <div key={timetableName}>
+            <h2 className="font-heading mt-12 scroll-m-20 pb-2 text-2xl font-semibold tracking-tight first:mt-0 mb-4">{timetableName}</h2>
+            <div className="flex mb-8 border justify-between">
+              {Object.entries(timetable).map(([day, dayData]) => (
+                <div key={day} className="flex flex-col border flex-grow">
+                  <span className="py-3 px-4 align-middle font-medium text-muted-foreground border-b text-center">{day}</span>
+                  {dayData.map((timeSlot) => (
+                    <span key={timeSlot.TimeTable.id} className="py-2 px-4 align-middle text-center">
+                      {timeSlot.Subject.subjectName}
+                    </span>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
     </>
   );
 }
--- src/pages/timetable/index.tsx
+++ src/pages/timetable/index.tsx
@@ -1,12 +1,13 @@
 import { useSession } from "next-auth/react";
 import { DashboardLayout } from "@/components/DashboardLayout";
-import type { NextPageWithLayout } from '~/pages/_app';
-import { api } from "~/utils/api";  
+import type { NextPageWithLayout } from "~/pages/_app";
+import { api } from "~/utils/api";
 import { LoadingTable } from "@/components/LoadingTable";
 import { TimeTable } from "@/components/TimeTable";
 import { Separator } from "@/components/ui/separator";
+import { Balancer } from "react-wrap-balancer";
 
-const Timetable:NextPageWithLayout = () => {
+const Timetable: NextPageWithLayout = () => {
   useSession({
     required: true,
     onUnauthenticated() {
@@ -15,13 +16,15 @@ const Timetable:NextPageWithLayout = () => {
   });
   const { data, isLoading } = api.timetable.getAll.useQuery();
   console.log("TIMETABLE: ", data);
-  
+
   return (
     <>
       <div className="space-y-2">
-        <h2 className="text-2xl font-bold tracking-tight"> Timetables </h2>
-        <p className="text-muted-foreground">
-          View your timetable here.
+        <h1 className="scroll-m-20 text-4xl font-bold tracking-tight">
+          Timetables
+        </h1>
+        <p className="text-lg text-muted-foreground">
+          <Balancer>View your timetable here.</Balancer>
         </p>
       </div>
       <Separator className="my-5" />
@@ -32,14 +35,10 @@ const Timetable:NextPageWithLayout = () => {
       )}
     </>
   );
-}
+};
 
 Timetable.getLayout = function getLayout(page: React.ReactElement) {
-  return (
-    <DashboardLayout>
-      {page}
-    </DashboardLayout>
-  );
+  return <DashboardLayout>{page}</DashboardLayout>;
 };
 
 export default Timetable;
