5a15fa9776084637f06c129d231890396cc8a089
akshar <94466270+blucin@users.noreply.github.com>
2023-06-05 11:51:02 +0000

    Fix import error

--- src/pages/api/restricted.ts
+++ src/pages/api/restricted.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "./auth/[...nextauth]";
+import { authOptions } from "~/server/auth";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
