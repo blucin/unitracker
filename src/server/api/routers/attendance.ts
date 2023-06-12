fc69df1ba2f83ceacb20271a09c6adbf579327c6
blucin <glaivesharp@gmail.com>
2023-06-12 19:04:34 +0530

    Finalize attendance route handling

--- src/server/api/routers/attendance.ts
+++ src/server/api/routers/attendance.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
-import { getByTimetableId } from "~/server/api/services/timetableService";
+import { getAttendanceByDateRangeWithDay } from "~/server/api/services/attendanceService";
+import { getSubjectCountByDateRange } from "~/server/api/services/timetableService";
+import _ from "lodash";
 
 export const attendanceRouter = createTRPCRouter({
   getByRange: protectedProcedure
@@ -12,8 +14,72 @@ export const attendanceRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      const timeTable = await getByTimetableId(ctx.db, input.timetableName, ctx.session.user.id);
-      console.log("TIMETABLE1:", timeTable);
-      return timeTable;
+      type resultSchema = {
+        lab: {
+          [key: string]: number;
+        },
+        theory: {
+          [key: string]: number;
+        }
+      }
+
+      // contains subjectName with it's attendance in % for lab and theory
+      const result:resultSchema = {
+        lab: {},
+        theory: {},
+      };
+
+      // actual data of subjects which were taken regardless of user attending them or not
+      const subjectByTimeTableId = await getSubjectCountByDateRange(
+        ctx.db,
+        input.timetableName,
+        ctx.session.user.id,
+        input.startDate,
+        input.endDate
+      );
+
+      // attendance data of subjects attended by user
+      const attendanceByTimeTableId = await getAttendanceByDateRangeWithDay(
+        ctx.db,
+        input.timetableName,
+        ctx.session.user.id,
+        input.startDate,
+        input.endDate
+      );
+      
+      // format data
+      const subjectResult = _.groupBy(subjectByTimeTableId, function (item) {
+        return item.subjectName;
+      });
+
+      const attendanceResult = _.groupBy(
+        attendanceByTimeTableId,
+        function (item) {
+          return item.subjectName;
+        }
+      );
+
+      // form result by comparing both objects and calculate % of attendance
+      // TODO: make subject name unique in db
+      _.map(subjectResult, (value, key) => {
+        _.map(attendanceResult, (value2, key2) => {
+          // if subject name matches  
+          if(key===key2) {
+            _.map(value2, (value3, key3) => {
+              _.map(value, (value4, key4) => {
+                if (value3.isLab === value4.isLab) {
+                  const percentage = Number(value3.count) / Number(value4.count) * 100;
+                  if (value3.isLab) {
+                    result.lab[key] = Math.round(percentage * 100) / 100;
+                  } else {
+                    result.theory[key] = Math.round(percentage * 100) / 100;
+                  }
+                }
+              })
+            })
+          }
+      })})
+
+      return result;
     }),
 });
