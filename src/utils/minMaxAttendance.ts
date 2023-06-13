9883ee3b2927230e6b6abd854bc461a8cc5827f0
blucin <glaivesharp@gmail.com>
2023-06-13 15:31:42 +0530

    Add jsdoc comment

--- src/utils/minMaxAttendance.ts
+++ src/utils/minMaxAttendance.ts
@@ -1,6 +1,36 @@
 import _ from "lodash";
 import type { RouterOutput } from "~/server/api/root";
 
+/**
+ * @description Calculates the minimum and maximum attendance of the given data
+ * @param data The attendance data
+ * @returns The minimum and maximum attendance name and % value for lab and theory separately
+ * @example
+ * const data = {
+ *  theory: {
+ *    "Subject 1": 80,
+ *    "Subject 2": 90,
+ *    "Subject 3": 70,
+ *  },
+ *  lab: {
+ *    "Subject 1": 80,
+ *    "Subject 2": 90,
+ *    "Subject 3": 70,
+ *  },
+ * };
+ * const result = calculateMinMaxAttendance(data);
+ * console.log(result);
+ * // {
+ * //   theoryMaxName: "Subject 2",
+ * //   labMaxName: "Subject 2",
+ * //   theoryMinName: "Subject 3",
+ * //   labMinName: "Subject 3",
+ * //   theoryMax: 90,
+ * //   labMax: 90,
+ * //   theoryMin: 70,
+ * //   labMin: 70,
+ * // }
+ */
 export function calculateMinMaxAttendance(
   data: RouterOutput["attendance"]["getByRange"] | undefined
 ) {
