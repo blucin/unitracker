d287b5c773ea46ea1f79d23bfa1d9bb13c1d9c22
blucin <glaivesharp@gmail.com>
2023-06-15 19:18:21 +0530

    WIP Fix1

--- components/DashboardLayout.tsx
+++ components/DashboardLayout.tsx
@@ -3,11 +3,11 @@ import { SiteHeader } from "@/components/SiteHeader";
 
 export function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative flex min-h-screen flex-col">
-      <SiteHeader />
+    <div className="relative flex flex-col">
+      <SiteHeader className="sticky top-0 z-40 border-b w-full" />
       <div className="flex-1 lg:grid grid-cols-6">
-        <DashboardNavBar className="border-r h-screen"/>
-        <main className="p-10 col-span-5">{children}</main>
+        <DashboardNavBar className="border-r h-screen fixed top-14"/>
+        <main className="p-10 col-span-5 relative">{children}</main>
       </div>
     </div>
   );
