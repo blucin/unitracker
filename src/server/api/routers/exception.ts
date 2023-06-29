2064ec35b374f1b25ec2d5051011a35097a4ecaf
blucin <glaivesharp@gmail.com>
2023-06-30 00:05:49 +0530

    Add addException route handler

--- src/server/api/routers/exception.ts
+++ src/server/api/routers/exception.ts
@@ -1,13 +1,25 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { holidays } from "~/drizzle/out/schema";
 import { eq } from "drizzle-orm";
+import { ExceptionFormSchema } from "~/types/formSchemas";
+import { addException } from "~/server/api/services/exceptionService";
 
 export const exceptionRouter = createTRPCRouter({
-  // by userId
   getAll: protectedProcedure.query(({ ctx }) => {
     return ctx.db
       .select()
       .from(holidays)
       .where(eq(holidays.userId, ctx.session.user.id));
   }),
+  addException: protectedProcedure
+    .input(ExceptionFormSchema)
+    .mutation(({ ctx, input }) => {
+      return addException(
+        ctx.db,
+        ctx.session.user.id,
+        input.exceptionName,
+        input.startDate,
+        input.endDate != undefined ? (input.isRange === "true" ? input.endDate : input.startDate) : input.startDate
+      );
+    }),
 });
