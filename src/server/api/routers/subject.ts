4ca7536ba5a975475e0bdf4b0075103ee03d2ecb
blucin <glaivesharp@gmail.com>
2023-06-30 00:05:32 +0530

    Add get simple subject details route handler

--- src/server/api/routers/subject.ts
+++ src/server/api/routers/subject.ts
@@ -1,8 +1,12 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import {
+  getAllSubjectsIdAndNameByUserId,
+  addSubject,
+} from "~/server/api/services/subjectService";
+import { SubjectFormSchema } from "~/types/formSchemas";
 
 export const subjectRouter = createTRPCRouter({
-  // by userId
   getAll: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.subject.findMany({
       where: {
@@ -10,13 +14,27 @@ export const subjectRouter = createTRPCRouter({
       },
     });
   }),
+  getSubjectNameSubjectIdAndHasLab: protectedProcedure.query(({ ctx }) => {
+    return getAllSubjectsIdAndNameByUserId(ctx.db, ctx.session.user.id);
+  }),
+  addSubjectByUserId: protectedProcedure
+    .input(SubjectFormSchema)
+    .mutation(({ input, ctx }) => {
+      return addSubject(
+        ctx.db,
+        ctx.session.user.id,
+        input.subjectName,
+        input.hasLab === "true" ? true : false,
+        input.subjectCode
+      );
+    }),
   deleteById: protectedProcedure
     .input(
       z.object({
         id: z.string(),
       })
     )
-    .mutation(({input, ctx})=> {
+    .mutation(({ input, ctx }) => {
       return ctx.prisma.subject.delete({
         where: {
           id: input.id,
