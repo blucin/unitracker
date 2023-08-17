aecc3cc99d627018ff60d7447a665731882d8f11
blucin <glaivesharp@gmail.com>
2023-08-17 11:08:31 +0530

    Remove state management (unused)

--- src/store/store.ts
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-import { type DashboardSlice, dashboardSlice } from "~/store/slices/dashboardSlice";
-
-const Store = create<DashboardSlice>()((...a)=>({
-  ...dashboardSlice(...a),
-}));
-
-export default Store;
