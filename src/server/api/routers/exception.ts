caff93475c65e3f72a20551fe9843e52625c58b1
blucin <glaivesharp@gmail.com>
2023-08-19 21:49:51 +0530

    Add route to get exception by date range

--- src/server/api/routers/exception.ts
+++ src/server/api/routers/exception.ts
@@ -5,8 +5,14 @@ import { ExceptionFormSchema } from "~/types/formSchemas";
 import {
   addException,
   deleteException,
+  getExceptionsByDateRange,
 } from "~/server/api/services/exceptionService";
 import { z } from "zod";
+import { mergeOverlappingDateRanges } from "~/utils/filterArrayofDayRanges";
+import { weekdayCount } from "~/utils/weekDayCount";
+import { type timeTable } from "~/drizzle/out/schema";
+import { type InferModel } from "drizzle-orm";
+type sqlDayType = InferModel<typeof timeTable>["dayName"];
 
 export const exceptionRouter = createTRPCRouter({
   getAll: protectedProcedure.query(({ ctx }) => {
@@ -14,6 +20,63 @@ export const exceptionRouter = createTRPCRouter({
       .select()
       .from(holidays)
       .where(eq(holidays.userId, ctx.session.user.id));
+  }),
+  getExceptionsByDateRange: protectedProcedure.input(
+    z.object({
+      startDate: z.date(),
+      endDate: z.date(),
+    })
+  ).query(async ({ ctx, input }) => {
+    const overlappingDateRangeArr = await getExceptionsByDateRange(
+      ctx.db,
+      ctx.session.user.id,
+      input.startDate,
+      input.endDate
+    );
+
+    const filteredDateRangeArr = mergeOverlappingDateRanges(
+      overlappingDateRangeArr.map((dateRange) => {
+        return {
+          start: new Date(dateRange.startDate),
+          end: new Date(dateRange.endDate),
+        };
+    }));
+
+    const dayCountByWeekday = filteredDateRangeArr.reduce((acc, dateRange) => {
+      const dayCount = weekdayCount(dateRange.start, dateRange.end);
+      for (const [key, value] of dayCount.entries()) {
+        acc.set(key, acc.get(key)! + value);
+      }
+      return acc;
+    }, new Map<sqlDayType, number>([
+      ["Sunday", 0],
+      ["Monday", 0],
+      ["Tuesday", 0],
+      ["Wednesday", 0],
+      ["Thursday", 0],
+      ["Friday", 0],
+      ["Saturday", 0],
+    ]));
+
+    return Array.from(dayCountByWeekday.entries()).map(([dayName, count]) => {
+      return {
+        dayName,
+        count,
+      };
+    });
+  }),
+  checkIfExceptionExist: protectedProcedure.input(
+    z.object({
+      date: z.date(),
+    })
+  ).query(async ({ ctx, input }) => {
+    const result = await getExceptionsByDateRange(
+      ctx.db,
+      ctx.session.user.id,
+      input.date,
+      input.date
+    );
+    return result.length === 0 ? false : true;
   }),
   addException: protectedProcedure
     .input(ExceptionFormSchema)
